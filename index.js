1ca745c76e84fcfad149860d6fc8d813521469f5
Corbin Page <corbin.page@gmail.com>
2019-07-29 17:50:56 -0400

    Removed the ABI references

--- index.js
+++ index.js
@@ -27,186 +27,10 @@ const server = express()
   .use((req, res) => res.send('Hello World!') )
   .listen(port, () => console.log(`HTTP listening on ${ port }`));
 
-
-
 const DAIAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'
 const MKRAddress = '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2'
 const GNTAddress = '0xa74476443119a942de498590fe1f2454d7d4ac0d'
 const transferTopic = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-
-// const DAIAbi = require('./data/dai.json')
-const DAIAbi = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: '_spender',
-        type: 'address'
-      },
-      {
-        name: '_value',
-        type: 'uint256'
-      }
-    ],
-    name: 'approve',
-    outputs: [
-      {
-        name: 'success',
-        type: 'bool'
-      }
-    ],
-    payable: false,
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: 'totalSupply',
-    outputs: [
-      {
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    type: 'function'
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: '_from',
-        type: 'address'
-      },
-      {
-        name: '_to',
-        type: 'address'
-      },
-      {
-        name: '_value',
-        type: 'uint256'
-      }
-    ],
-    name: 'transferFrom',
-    outputs: [
-      {
-        name: 'success',
-        type: 'bool'
-      }
-    ],
-    payable: false,
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: '_owner',
-        type: 'address'
-      }
-    ],
-    name: 'balanceOf',
-    outputs: [
-      {
-        name: 'balance',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    type: 'function'
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: '_to',
-        type: 'address'
-      },
-      {
-        name: '_value',
-        type: 'uint256'
-      }
-    ],
-    name: 'transfer',
-    outputs: [
-      {
-        name: 'success',
-        type: 'bool'
-      }
-    ],
-    payable: false,
-    type: 'function'
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: '_owner',
-        type: 'address'
-      },
-      {
-        name: '_spender',
-        type: 'address'
-      }
-    ],
-    name: 'allowance',
-    outputs: [
-      {
-        name: 'remaining',
-        type: 'uint256'
-      }
-    ],
-    payable: false,
-    type: 'function'
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        name: '_from',
-        type: 'address'
-      },
-      {
-        indexed: true,
-        name: '_to',
-        type: 'address'
-      },
-      {
-        indexed: false,
-        name: '_value',
-        type: 'uint256'
-      }
-    ],
-    name: 'Transfer',
-    type: 'event'
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        name: '_owner',
-        type: 'address'
-      },
-      {
-        indexed: true,
-        name: '_spender',
-        type: 'address'
-      },
-      {
-        indexed: false,
-        name: '_value',
-        type: 'uint256'
-      }
-    ],
-    name: 'Approval',
-    type: 'event'
-  }
-]
-
-const web3Contract = new web3.eth.Contract(DAIAbi, DAIAddress)
-const daggerContract = dagger.contract(web3Contract)
 
 // Listen for every DAI token transfer occurs
 dagger.on(`confirmed:log/${DAIAddress}/filter/${transferTopic}/#`, result => {
@@ -222,23 +46,17 @@ dagger.on(`confirmed:log/${DAIAddress}/filter/${transferTopic}/#`, result => {
 })
 
 // // Listen for every MKR token transfer occurs
-// dagger.on(`confirmed:log/${MKRAddress}/filter/${transferTopic}/#`, result => {
-//   const tokenSymbol = 'MKR'
-//   const tokenAmount = getTransferAmountFromLogs(result)
-//   console.log(`Amount: ${tokenAmount} ${tokenSymbol}`)
+dagger.on(`confirmed:log/${MKRAddress}/filter/${transferTopic}/#`, result => {
+  const tokenSymbol = 'MKR'
+  const tokenAmount = getTransferAmountFromLogs(result)
+  console.log(`Amount: ${tokenAmount} ${tokenSymbol}`)
 
-//   triggerFlow('whale-mkr-tweeting', {
-//     amount: tokenAmount,
-//     tokenSymbol: tokenSymbol,
-//     transactionHash: result.transactionHash
-//   })
-// })
-
-// get new block as soon as it gets created
-// dagger.on("latest:block", function(result) {
-//   console.log("New block created: ", result);
-// });
-//
+  triggerFlow('whale-mkr-tweeting', {
+    amount: tokenAmount,
+    tokenSymbol: tokenSymbol,
+    transactionHash: result.transactionHash
+  })
+})
 
 function getTransferAmountFromLogs(logData) {
   const inputs = [
