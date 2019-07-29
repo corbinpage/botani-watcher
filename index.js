426f9cc5f626b8f9dbdbe2703bb966aaf51f2525
Corbin Page <corbin.page@gmail.com>
2019-07-29 17:09:37 -0400

    Toubleshooting deployment on Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Web3 = require("web3")
 const web3 = new Web3()
 const Dagger = require("eth-dagger")
 const BigNumber = require("bignumber.js")
-// const express = require('express');
+const express = require('express');
 
 const AWS = require('aws-sdk')
 
@@ -20,11 +20,10 @@ const port = process.env.PORT || 8000
 //   .listen(port, () => console.log(`HTTP listening on ${ port }`));
 
 // connect to Dagger ETH main network (network id: 1) over web socket
-// const options = [{ host: host, port: port }]
-const options = []
+const options = [{ host: host, port: port }]
+// const options = []
 const dagger = new Dagger(
   "wss://mainnet.dagger.matic.network", 
-  // "mqtts://mainnet.dagger.matic.network", 
   options
 ) // dagger server
 
